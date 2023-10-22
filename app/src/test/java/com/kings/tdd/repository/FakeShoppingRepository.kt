b502688043717e1bd8ccb5821cbdf3600271f901
kingsleyjohn <nwankwo.kingsleyjohn@gmail.com>
2023-10-22 17:32:43 +0100

    create fake repository

---
+++ app/src/test/java/com/kings/tdd/repository/FakeShoppingRepository.kt
@@ -0,0 +1,68 @@
+package com.kings.tdd.repository
+
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.kings.tdd.data.local.ShoppingItem
+import com.kings.tdd.data.remote.response.ImageResponse
+import com.kings.util.Resource
+
+class FakeShoppingRepository : ShoppingRepository {
+
+    private val shoppingItems = mutableListOf<ShoppingItem>()
+
+    private val observableShoppingItems = MutableLiveData<List<ShoppingItem>>(shoppingItems)
+    private val observableTotalPrice = MutableLiveData<Float>()
+
+    private var shouldReturnNetworkError = false
+
+    fun setShouldReturnNetworkError(value: Boolean) {
+        shouldReturnNetworkError = value
+    }
+
+    private fun refreshLiveData() {
+        observableShoppingItems.postValue(shoppingItems)
+        observableTotalPrice.postValue(getTotalPrice())
+    }
+
+    private fun getTotalPrice(): Float {
+        return shoppingItems.sumByDouble { it.price.toDouble() }.toFloat()
+    }
+
+    override suspend fun insertShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.add(shoppingItem)
+        refreshLiveData()
+    }
+
+    override suspend fun deleteShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.remove(shoppingItem)
+        refreshLiveData()
+    }
+
+    override fun observeAllShoppingItems(): LiveData<List<ShoppingItem>> {
+        return observableShoppingItems
+    }
+
+    override fun observeTotalPrice(): LiveData<Float> {
+        return observableTotalPrice
+    }
+
+    override suspend fun searchShoppingItem(searchQuery: String): Resource<ImageResponse> {
+        return if(shouldReturnNetworkError) {
+            Resource.Error(error = "Error")
+        } else {
+            Resource.Success(ImageResponse(listOf(), 0, 0))
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
